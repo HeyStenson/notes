b9c28306a92e97bcc1c07b69e82f3f342a46e2f8
HeyStenson <h.n.stenson@gmail.com>
2015-08-18 09:29:51 -0700

    changes from yesterday

--- week-02/day-01-arrays+Iterators/dusk-iteratorsLab/starterCode/iterators.js
+++ week-02/day-01-arrays+Iterators/dusk-iteratorsLab/starterCode/iterators.js
@@ -1,10 +1,17 @@
 //where our iterators live
 var o_o = {
   each: function(list, callback) {
-    //code here...
+    for (var i = 0; i < list.length; i++){
+      var element = list[i];
+      callback(element);
+    }
   },
   map: function(list, callback){
-    //code here...
+    var output = [];
+      for (var i = 0; i < list.length; i++){
+        output[i] = callback(list[i]);
+      }
+      return output;
   },
   filter: function(list, callback) {
     var output = [];
